e5834375056c354526ec4f989cdc3938f3b9967f
mjfutera <mojasuperfirma@gmail.com>
2022-12-19 16:20:53 +0100

    v. 1.004
    App correctly check answer and change style for correct/incorrect

--- scripts.js
+++ scripts.js
@@ -1,26 +1,44 @@
 const getData = async (url) => await fetch(url).then(r => r.json());
 
-const answers = object => {
+const showQuestion = async () => {
+    const data = await getData('https://api.michalfutera.pro/QuizApp/database/getQuestion');
+    document.getElementById("question").innerText = data['question'];
+    document.getElementById("questionCategory").innerText = "Category: "+data['category'];
     document.getElementById("answers").innerHTML = '';
-    let answers = '';
-    for (const [key, value] of Object.entries(object)) {
-        answers = answers + `            
-        <div class="answer" id="${key}">
-            <h2>${value}</h2>
-        </div>`;
-    document.getElementById("answers").innerHTML = answers;
+    const answersArray = [];
+    for (const [key, value] of Object.entries(data['answers'])) {
+        answersArray.push(`            
+        <div class="answer" id="${key}" onclick="checkAllAnswers('${key}', '${data['correct']}', '${Object.keys(data['answers'])}')">
+            <h2 id="text-${key}">${value}</h2>
+        </div>`);
+    }
+    document.getElementById("answers").innerHTML = answersArray.join('');
+}
+
+const checkOneAnswer = (chosen, correct, id) => {
+    if (chosen === correct) {
+        if (chosen == id) {
+            document.getElementById(id).className = "correctAnswer";
+        } else {
+            document.getElementById(id).className = "notUsedAnswer";
+        }
+    } else { // incorrect answer
+        if (id === correct) {
+            document.getElementById(id).className = "correctAnswer";
+            document.getElementById(id).removeAttribute("onclick");
+        } else if (id === chosen) {
+            document.getElementById(id).className = "incorrectAnswer"; 
+            document.getElementById(id).removeAttribute("onclick");
+        } else {
+            document.getElementById(id).className = "notUsedAnswer";
+            document.getElementById(id).removeAttribute("onclick");
+        }
     }
 }
 
-const showQuestion = async () => {
-    const data = await getData('https://api.michalfutera.pro/QuizApp/database/getQuestion');
-    console.log(data);
-    document.getElementById("question").innerText = data['question'];
-    document.getElementById("questionCategory").innerText = "Category: "+data['category'];
-    answers(data['answers']);
+const checkAllAnswers = (chosen, correct, all) => {
+    all.split(",").forEach(r => checkOneAnswer(chosen, correct, r));
 }
-
-
 
 showQuestion();
 document.getElementById("getQuestion").addEventListener("click", showQuestion);
