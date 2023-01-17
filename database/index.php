6b53037b3c293dd3caa19df4cbe700c2401fc385
Michał Futera <100314711+mjfutera@users.noreply.github.com>
2023-01-17 20:10:48 +0100

    v. 1.012
    Added API structure for other features

--- database/index.php
+++ database/index.php
@@ -7,13 +7,15 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
     header("Access-Control-Allow-Headers: Content-Type");
 
+    $header = apache_request_headers();
+
     $database = 'database.db';
     $currentTime = date("Y/m/d h:i:sa");
     $url = URLarray();
     $i = 3;
 
     if($_SERVER['REQUEST_METHOD'] == 'GET') { 
-        if($url[$i] == 'getQuestion') {
+        if($url[$i] == 'getQuestion') { // gets questions
             if(isset($_GET['category'])) {
                 $sql = "SELECT 
                 questions.question AS question, 
@@ -29,9 +31,9 @@
                 $result['answers'] = json_decode($SQLresult['answers']);
                 $result['correct'] = $SQLresult['correct'];
                 $result['category'] = $SQLresult['category'];
+                $result['headers'] = $header;
                 echo json_encode($result);
                 exit();
-
             } else {
                 $sql = "SELECT 
                 questions.question AS question, 
@@ -48,23 +50,24 @@
                 $result['category'] = $SQLresult['category'];
                 echo json_encode($result);
                 exit();
-            }
-        }
-        if($url[$i] == 'getCategories') {
+            }}
+        if($url[$i] == 'getCategories') { // gets category list
             $sql = "SELECT * FROM categories";
             $SQLresult['categories'] = connectSQLite($sql, $database);
             echo json_encode($SQLresult);
-            exit();
-        }
-        if($url[$i] == 'getResults') {
+            exit();}
+        if($url[$i] == 'getResults') { // gets result list
             $sql = "SELECT name, result, result_time FROM results ORDER BY result DESC LIMIT 10";
             $result['results'] = connectSQLite($sql, $database);
-            echo json_encode($result);
-        }
-    }
+            echo json_encode($result);}}
+        // if($url[$i] == 'getAwaitingQuestion') {} // gets awaiting questions from DB to be approved/ modified or deleted by admin. Require Password
+        // if($url[$i] == 'getAwaitingCategory') {} // gets awaiting categories from DB to be approved/ modified or deleted by admin. Require Password
+        // if($url[$i] == 'getStats') {} // shows statistics for categories
+        // if($url[$i] == 'checkPassword') {} // checks password for admin panel
+        
     if($_SERVER['REQUEST_METHOD'] == 'POST') { 
         $data = json_decode(file_get_contents('php://input'), true);
-        if($url[$i] == 'postQuestion') {
+        if($url[$i] == 'postAwaitingQuestion') { //post new question do database. !NEED TO IMPROVE DATA VERIFICATION and CHANGE FRONTEND part. old postQuestion. 
             $checking['categoryCheck'] = fullCategoryCheck($data, $database);
             $checking['questionCheck'] = questionCheck($data);
             $checking['answerCheck'] = answerObjectCheck($data);
@@ -84,13 +87,13 @@
                 $result['fields'] = $checking;
                 http_response_code(201);
                 echo json_encode($result);
-            } 
-        }
+            }}
 
-        if($url[$i] == 'postResult') {
+
+        if($url[$i] == 'postResult') { //post result do database. !NEED TO IMPROVE DATA VERIFICATION
             $name = $data['name'];
             $result = $data['result'];
-            $checking['resultCheck'] = !SQLincection($result) && gettype($data['name'] ;
+            $checking['resultCheck'] = !SQLincection($result) && gettype($result) ==  ;
             $checking['nameCheck'] = !SQLincection($name) && gettype($data['name'] == 'string') && strlen($data['name'])>0;
             if ($checking['nameCheck'] && $checking['resultCheck']) {
                 $sql = "
@@ -98,7 +101,7 @@
                     name,
                     result,
                     result_time
-                ) VALUES (
+                ) VALUES ( 
                     '$name',
                     '$result',
                     '$currentTime'
@@ -113,8 +116,21 @@
                 $finalResult['status'] = "Failure";
                 $finalResult['message'] = "One or more fields are incorrect";
                 $finalResult['fields'] = $checking;
-                echo json_encode($finalResult);
-            }
-        }
+                echo json_encode($finalResult);}} 
+        // if($url[$i] == 'postNewQuestion') {} // adds question to database, after approvement by admin. Require Password
+        // if($url[$i] == 'postNewCategory') {} // adds category to database, after approvement by admin. Require Password
     }
+    // if($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    //     if($url[$i] == 'editQuestion') {} // edit question in database. Require Password
+    //     if($url[$i] == 'editCategory') {} // edit category in database. Require Password
+    //     if($url[$i] == 'editAwaitingQuestion') {} // edit question in database. Require Password
+    //     if($url[$i] == 'editAwaitingCategory') {} // edit category in database. Require Password
+    // } 
+    // if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    //     if($url[$i] == 'deleteQuestion') {} // delete question in database. Require Password
+    //     if($url[$i] == 'deleteAwaitingQuestion') {} // delete awaiting question in database. Require Password
+    //     if($url[$i] == 'deleteCategory') {} // delete category in database. Require Password
+    //     if($url[$i] == 'deleteAwaitingQuestion') {} // delete awaiting category in database. Require Password
+    //     if($url[$i] == 'deleteResult') {} // delete awaiting category in database. Require Password
+    // }
 ?>
